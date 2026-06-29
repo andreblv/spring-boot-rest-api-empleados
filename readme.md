19c25234dce38114ae30a74ac904974df96d63a5
andreblv <andrelopezbrayan@gmail.com>
2026-06-28 22:55:30 -0500

    iniciando repo

---
+++ readme.md
@@ -0,0 +1,4 @@
+#  API REST Empleados
+
+- API RESTful para la gestión de empleados, usando arquitectura MVC, Spring JPA, Security, Lombok, Web.
+
---
+++ src/test/java/com/empresa/demo/EmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

653a1f0bf8dd0e260b94cdbe7f6601c907321e03
andreblv <andrelopezbrayan@gmail.com>
2026-07-01 00:21:50 -0500

    feat(entity): añadir clase empleado

---
+++ src/main/java/com/empresa/entity/Empleado.java
@@ -0,0 +1,30 @@
+package com.empresa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table (name = "empleados")
+
+@Data
+public class Empleado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_empleado;
+	
+	private String nombre;
+	private String apellido;
+	private String num_dni;
+	private String cargo_puesto;
+	private String departamento;
+	private String correo_emp;
+	private String num_tel;
+	
+	private String estado;
+
+}

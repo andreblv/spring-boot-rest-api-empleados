17ad31865276754f6faac12d5069aaf57e00a1f1
andreblv <andrelopezbrayan@gmail.com>
2026-07-05 12:29:10 -0500

    feat(dto): agregar EmpleadoResponseDTO

---
+++ src/main/java/com/empresa/dto/EmpleadoResponseDTO.java
@@ -0,0 +1,21 @@
+package com.empresa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+  
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+   
+public class EmpleadoResponseDTO {
+	
+	private String nombre;
+	private String apellido;
+	private String cargo_puesto;
+
+}
+	

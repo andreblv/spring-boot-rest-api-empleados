f45f08416b9793e4fc7ab6661c25e644c932e58b
andreblv <andrelopezbrayan@gmail.com>
2026-07-05 18:15:59 -0500

    feat(converter): convertir entre entity y dto

--- src/main/java/com/empresa/converter/EmpleadoConverterDTO.java
+++ src/main/java/com/empresa/converter/EmpleadoConverterDTO.java
@@ -10,18 +10,11 @@ public class EmpleadoConverterDTO {
 	
 	public static Empleado toEntity(EmpleadoRequestDTO dto){
 		
-		Empleado empleado = new Empleado();
-		
-		empleado.setNombre(dto.getNombre());
-		empleado.setApellido(dto.getApellido());
-		empleado.setNum_dni(dto.getNum_dni());
-		empleado.setCargo_puesto(dto.getCargo_puesto());
-		empleado.setDepartamento(dto.getDepartamento());
-		empleado.setCorreo_emp(dto.getCorreo_emp());
-		empleado.setNum_tel(dto.getNum_tel());
-		empleado.setEstado(dto.getEstado());
-		
-		return empleado;
+        Empleado empleado = new Empleado();
+
+        updateEntity(empleado, dto);
+
+        return empleado;
 	}
 
 	
@@ -33,5 +26,20 @@ public class EmpleadoConverterDTO {
 			empleado.getCargo_puesto()
 		);
 	}
+	
+	
+    public static void updateEntity(Empleado empleado, EmpleadoRequestDTO dto) {
+
+        empleado.setNombre(dto.getNombre());
+        empleado.setApellido(dto.getApellido());
+        empleado.setNum_dni(dto.getNum_dni());
+        empleado.setCargo_puesto(dto.getCargo_puesto());
+        empleado.setDepartamento(dto.getDepartamento());
+        empleado.setCorreo_emp(dto.getCorreo_emp());
+        empleado.setNum_tel(dto.getNum_tel());
+        empleado.setEstado(dto.getEstado());
+
+    }
+	
 
 }

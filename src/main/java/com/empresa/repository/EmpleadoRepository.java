7ef4d2001661b8602c77f9175b2a28d7b489d35f
andreblv <andrelopezbrayan@gmail.com>
2026-07-09 19:26:32 -0500

    feat(repo): agregar métodos para buscar por nombre y cargo

--- src/main/java/com/empresa/repository/EmpleadoRepository.java
+++ src/main/java/com/empresa/repository/EmpleadoRepository.java
@@ -2,9 +2,11 @@ package com.empresa.repository;
 
 import com.empresa.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long>  {
-
 	
+	List<Empleado> findByNombreContainingIgnoreCase(String nombre);
+	List<Empleado> findByCargoPuestoContainingIgnoreCase(String cargoPuesto);
 }
+	

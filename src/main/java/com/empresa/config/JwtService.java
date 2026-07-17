85a8a8a1933e5257ee998a3fed8cc6b8c761fb0b
andreblv <andrelopezbrayan@gmail.com>
2026-07-17 18:16:23 -0500

    feat(config): añadir jwtservice

---
+++ src/main/java/com/empresa/config/JwtService.java
@@ -0,0 +1,57 @@
+package com.empresa.config;
+
+
+import java.util.Date;
+import java.util.function.Function;
+import javax.crypto.SecretKey;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+@Service
+public class JwtService {
+
+	
+	//private static final String SECRET_KEY = "clave_Secreta";
+	@Value("${jwt.secret}")
+	private String SECRET_KEY;
+	
+	public String getUsername(String token) {
+		return getClaim(token, Claims::getSubject);
+	}
+	
+	public <T> T getClaim(String token, Function<Claims, T> claimsResolver){
+		final Claims claims = getAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	
+	public Claims getAllClaims(String token) {
+		return Jwts.parser()
+		    .verifyWith(getSignInKey())
+		    .build()
+		    .parseSignedClaims(token)
+		    .getPayload();
+	}
+	
+	private SecretKey getSignInKey() {
+	    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+	    return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	public boolean validateToken(String token, UserDetails userDetails) {
+		final String username = getUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return getExpiration(token).before(new Date());
+	}
+	
+	private Date getExpiration(String token) {
+		return getClaim(token, Claims::getExpiration);
+	}
+}
